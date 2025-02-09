f44faa2325411df31ae5e5e981790464a1100c21
Konstantin Berg <163849606+it2konst@users.noreply.github.com>
2025-02-09 13:41:19 +0700

    feat: component CardButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import Button from "@/components/Button/Button";
 import JournalItem from "@/components/JournalItem/JournalItem";
+import CardButton from "@/components/CardButton/CardButton";
 
 function App() {
   return (
     <>
-      <div>Привет!!!</div>
+      <h1>Заголовок</h1>
+      <p>Привет!!!</p>
       <Button>Кнопка</Button>
-      <JournalItem title="Заголовок" post="Текст" date={new Date()} />
+
+      <CardButton>
+        <JournalItem title="Заголовок" post="Текст" date={new Date()} />
+      </CardButton>
     </>
   );
 }
