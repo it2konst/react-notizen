6e873339dcecbbd42dda6c653508f3e19e3c26a9
Konstantin Berg <163849606+it2konst@users.noreply.github.com>
2025-02-09 09:55:15 +0700

    feat: component JournalItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Button from "@/components/Button/Button";
+import JournalItem from "@/components/JournalItem/JournalItem";
 
 function App() {
   return (
     <>
       <div>Привет!!!</div>
+      <Button>Кнопка</Button>
+      <JournalItem title="Заголовок" post="Текст" date={new Date()} />
     </>
   );
 }
